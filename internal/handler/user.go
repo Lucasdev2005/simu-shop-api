ffe19de0b0210dcd5b996b361d61798397e90cef
Lucas Moreira Nunes <lucas2005dev@gmail.com>
2024-03-31 22:09:29 -0300

    Update user.go

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -2,14 +2,13 @@ package handler
 
 import (
 	"simushop/internal/core"
-	"simushop/internal/entity"
 )
 
 type CreateUser struct {
 }
 
 func (h handler) CreateUser(request core.Request) (core.Success, core.Fail) {
-	var user entity.User
+	var user CreateUser
 	request.Body(&user)
 
 	return core.Success{
