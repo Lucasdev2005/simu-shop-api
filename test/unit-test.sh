11a89bee6b11d6611f1b787142b84d89b0b55024
Lucas Moreira Nunes <lucasmoreira@etssoft.com,br>
2024-04-07 21:31:03 -0300

    Revert "pipeline."
    This reverts commit ef6a5f495131316a08ac74946075cff41029a864.
    
    Conflicts:
    	.github/workflows/go.yml

---
+++ test/unit-test.sh
@@ -0,0 +1,12 @@
+# handler
+echo "--------------------------------------------------------------------------------------"
+echo "Running unit tests from handler"
+echo "--------------------------------------------------------------------------------------"
+go test ./internal/handler
+
+# Repository
+echo "--------------------------------------------------------------------------------------"
+echo "Running unit tests from repository"
+echo "--------------------------------------------------------------------------------------"
+rm internal/repository/gorm.db
+go test ./internal/repository
