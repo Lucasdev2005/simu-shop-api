119fe4911570880bac3aba354217eada39bf1d27
Lucas Moreira Nunes <lucasmoreira@etssoft.com,br>
2024-04-10 21:25:57 -0300

    Update product-handler_test.go

--- internal/handler/product-handler_test.go
+++ internal/handler/product-handler_test.go
@@ -1,1 +1,31 @@
 package handler_test
+
+import (
+	"simushop/internal/core"
+	"simushop/internal/dto"
+	"testing"
+)
+
+func TestRequiredFields(t *testing.T) {
+	t.Log("testing required fields.")
+
+	res := handlerInstance.CreateProduct(core.Request{
+		Body: func(obj any) error {
+			createProductDTO, ok := obj.(*dto.CreateProductDTO)
+
+			if ok {
+				createProductDTO.ProductName = "produto de teste"
+				createProductDTO.ProductCentimeterHeight = 2.6
+				createProductDTO.ProductCentimeterLength = 2.6
+				createProductDTO.ProductCentimeterWidth = 2.6
+				createProductDTO.ProductDescription = "Produto de teste"
+				createProductDTO.ProductValue = 2000.5
+				createProductDTO.ProductKgWeitght = 456.2
+			}
+
+			return nil
+		},
+	})
+
+	Ok(res, t)
+}
